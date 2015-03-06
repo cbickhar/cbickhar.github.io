ae48a73210a24f270016275bf1f9454a9129462c
cbickhar <cbickhar@pratt.edu>
2015-03-06 17:54:06 +0000

    My commit message

--- project_3_js/myscript.js
+++ project_3_js/myscript.js
@@ -1,14 +1,55 @@
-var someJunk=["*","*","*","|","###","****"];
+var item = "^";
+
+for (var myVar = 0; myVar < 10; myVar++) {
+  item = item + "^^^";
+  console.log(item);
+  var item2 = "|||";
+  
+    for (var myVar2=10; myVar2 < 15; myVar2+= 2) {
+                item2 = item2 + "v";
+    }
+    
+    for (var myVar2=10; myVar2 < 20; myVar2+= 2) {
+                item2 = item2 + "V";
+    }
+    
+    for (var myVar2=10; myVar2 < 25; myVar2+= 2) {
+                item2 = item2 + "v";
+    }
+    
+    for (var myVar2=10; myVar2 < 30; myVar2+= 2) {
+                item2 = item2 + "V";
+    }
+  
+    for (var myVar2=0; myVar2 < 40; myVar2++) {
+                item2 = item2 + "v";
+    if (myVar2 == 30) {
+      item2 = "v";
+    
+    }
 
 
-
+    console.log(item2);
+  }
+}
+   
+   
+   
+   
+   
+        
 var star = "*";
 
-for (var i =0; i < 10; i++)
-        {
-            for (var i=0; i < 200; i++)
-            {
-                console.log(someJunk);
-            }
-            console.log();
-        }
+for (var myVar = 1; myVar <= 10; myVar++) {
+  star = star + "*";
+  console.log(star);
+  var star2 = "R";
+
+  for (var myVar2 = 0; myVar2 < 20; myVar2++) {
+    star2 = star2 + "R";
+    if (myVar2 == 10) {
+      star2 = "R";
+    }
+    console.log(star2);
+  }
+}
