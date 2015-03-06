212480842abf862f5c05695f87aaed1244a68bea
cbickhar <cbickhar@pratt.edu>
2015-03-06 05:38:08 +0000

    My commit message

---
+++ project_3_javascriptexcersize/myscript.js
@@ -0,0 +1,14 @@
+var someJunk=["*","*","*","|","###","****"];
+
+
+
+var star = "*";
+
+for (var i =0; i < 10; i++)
+        {
+            for (var i=0; i < 200; i++)
+            {
+                console.log(someJunk);
+            }
+            console.log();
+        }
