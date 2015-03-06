555fc9e1b63ed4594d38e0741d4ddedf224342e0
cbickhar <cbickhar@pratt.edu>
2015-03-06 19:07:12 +0000

    My commit message

--- jQuery_exercize/script.js
+++ jQuery_exercize/script.js
@@ -1,6 +1,5 @@
 $.remove("pirate");
 
-$.show("gradient");
 
 /*
 
@@ -29,5 +28,6 @@ Now that we have stored the status of our button, let's show the user an image i
 If the user has activated the button, toggle the visibility of id "cat" to reveal our animated leopard.
 
 */
+
 
 
