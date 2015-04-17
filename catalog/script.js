de97688ebad05dc5e0079a7af8d7af83ce6a72ce
cbickhar <cbickhar@pratt.edu>
2015-04-17 00:40:06 +0000

    My commit message

--- catalog/script.js
+++ catalog/script.js
@@ -1,104 +1,54 @@
-[
-    {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#444444"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "saturation": "-0"
-            },
-            {
-                "lightness": "0"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "saturation": "0"
-            },
-            {
-                "lightness": "0"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#603913"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    }
-];
+directionsService = new google.maps.DirectionsService();
+directionsDisplay = new google.maps.DirectionsRenderer();
+
+var UK = new google.maps.LatLng(53.409532, -2.010498);
+var IT = new google.maps.LatLng(42.745334, 12.738430);
+
+var noStreetNames = [{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [{
+        visibility: "on"}]}];
+
+hideLabels = new google.maps.StyledMapType(noStreetNames, {
+    name: "hideLabels"
+});
+
+
+var myOptions = {
+    zoom: 1,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: UK
+}
+
+var showPosition = function(position) {
+    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var marker = new google.maps.Marker({
+        position: userLatLng,
+        title: 'Your Location',
+        draggable: true,
+        map: map
+    });
+
+
+
+
+
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
